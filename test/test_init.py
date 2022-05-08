c65edd4c4a0e4619754a38e972899134ec2e3691
happyxianyu <happyxianyu623@outlook.com>
2022-05-08 12:11:47 -0400

    init

---
+++ test/test_init.py
@@ -0,0 +1,12 @@
+from cbutil import *
+import asyncio
+import logging
+
+proj_root_path = Path(__file__).prnt.prnt
+test_tmp_path = proj_root_path/Path('tmp/test')
+
+async def atest_init():
+    logging.info('init success')
+
+def test_init():
+    asyncio.run(atest_init())
